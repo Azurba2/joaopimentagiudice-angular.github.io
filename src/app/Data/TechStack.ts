97b6c0624a9e92eda11f318476093eeb2b891c0b
azurba2 <joaoh.pimenta.giudice@hotmail.com>
2023-06-03 15:41:00 -0600

    update things in chronological order

--- src/app/Data/TechStack.ts
+++ src/app/Data/TechStack.ts
@@ -4,6 +4,8 @@ export class TechStack{
         "https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white",
         "https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white",  
         "https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white",
+        "https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white",
+        "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB",  
         "https://img.shields.io/badge/azure-%230072C6.svg?style=for-the-badge&logo=azure-devops&logoColor=white", 
         "https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white",
         "https://img.shields.io/badge/SQL%20Server-CC2927?style=for-the-badge", 
@@ -12,10 +14,7 @@ export class TechStack{
         "https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white",  
         "https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E", 
         "https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white", 
-        "https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white", 
         "https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white", 
         "https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens", 
-        "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB", 
-    ]
-    
-    }
+    ]    
+}
