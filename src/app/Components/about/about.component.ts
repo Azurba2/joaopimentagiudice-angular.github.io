778475caeda78a8874a8079e30d8bdba27f375c5
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-09-08 11:22:39 -0600

    add resumes and download option

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,8 +16,9 @@ export class AboutComponent {
   index : number = 0;
   @Input() themeColor : string | undefined;
   techBadges : Array<string>
+  showLanguageButtons = false;
 
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, private _techBadges : TechStack, private snackBar : MatSnackBar) {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, private _techBadges : TechStack, private snackBar : MatSnackBar, private http: HttpClient) {
     this.matIconRegistry.addSvgIcon('linkedin', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/linkedin.svg'))
     this.matIconRegistry.addSvgIcon('facebook', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/facebook.svg'))
     // this.matIconRegistry.addSvgIcon('Github', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/github.svg'))
@@ -37,6 +39,24 @@ export class AboutComponent {
       verticalPosition: 'top'
     });
     window.open(link);
+  }
+
+  toggleLanguageButtons() {
+    this.showLanguageButtons = !this.showLanguageButtons;
+  }
+
+  downloadResume(language : string){
+    const fileUrl = `/assets/documents/JohnHenry_resume23_${language}.pdf`; 
+    this.http.get(fileUrl, { responseType: 'blob' }).subscribe((data: Blob) => {
+      const blob = new Blob([data], { type: 'application/octet-stream' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'JohnHenry_23.pdf';
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    });
   }
 
 }
